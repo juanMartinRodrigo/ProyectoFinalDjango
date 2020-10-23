be32eefc2f679e88de289df31db4024189917317
juanMartinRodrigo <50252728+juanMartinRodrigo@users.noreply.github.com>
2020-10-23 16:40:36 -0300

    admin y models hecho

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from core.models import *
+
+class EquipoAdmin(admin.ModelAdmin):
+    search_fields =['club']
+    list_display = ['club','division','cantJugadores','amonestados','expulsados',]
+    fieldsets = (
+            ('Nombre de club',  {'fields': ('club',)}),
+            ('Datos sobre el equipo',  {'fields': ('division','cantJugadores','amonestados','expulsados',)}),
+    )   
+    
+class JugadorAdmin(admin.ModelAdmin):
+    search_fields = ['nombre','apellido','dni']
+    fieldsets = (
+            ('Datos de Personal', {'fields': ('nombre', 'apellido', 'dni',)}),
+            ('Contacto ', {'fields': ('tel',)}),
+            ('Datos del Jugador', {'fields': ('posicion','gym','asistenciaComp','amonestado','expulsado',)}),
+    )
+    list_display = ['nombre', 'apellido', 'dni','tel','posicion','gym','asistenciaComp','amonestado','expulsado',]
+    
+class EntrenadorAdmin(admin.ModelAdmin):
+	list_display = ['nombre', 'apellido', 'dni','telefono']
+	fieldsets = (
+            ('Datos de Personal', {'fields': ('nombre', 'apellido', 'dni',)}),
+            ('Contacto ', {'fields': ('telefono',)}),
+    )
+	search_fields = ['nombre','apellido','dni']
+
+class PartidoAdmin(admin.ModelAdmin):
+    search_fields = ['idPartido']
+    fieldsets = (
+            ('ID', {'fields': ('idPartido',)}),
+            ('Cantidad de try', {'fields': ('tri',)}),
+            ('Datos de Partido', {'fields': ('numLine','numScrum','numPenales','numConversiones','numDrop','numTacles','numMaul','numRuck','cantPases',)}),
+    )
+    list_display = ['tri','numLine','numScrum','numPenales','numConversiones','numDrop','numTacles','numMaul','numRuck','cantPases',]
+    
+class ErrorAdmin(admin.ModelAdmin):
+    search_fields = ['idError']
+    fieldsets = (
+            ('ID', {'fields': ('idError',)}),
+            ('Errores de los Fowers', {'fields': ('numLinePerdidos','numScrumPerdidos','numMaulErrados','numRuckErrados',)}),
+            ('Errores de los 3/4', {'fields': ('numConversionesErrados','numDropErrados',)}),
+            ('Errores de el Equipo', {'fields': ('numPenalesEnContra','numPelotasCaidas','numPaseFoword','numTaclesErrados',)}),
+    )
+    list_display = ['idError','numPenalesEnContra','numPelotasCaidas','numPaseFoword','numTaclesErrados','numConversionesErrados','numDropErrados','numLinePerdidos','numScrumPerdidos','numMaulErrados','numRuckErrados',]
+
+
 
 # Register your models here.
+admin.site.register(Jugador,JugadorAdmin);
+admin.site.register(Equipo,EquipoAdmin);
+admin.site.register(Error,ErrorAdmin);
+admin.site.register(Partido,PartidoAdmin);
+admin.site.register(Estadistica,);
--- core/models.py
+++ core/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
 # Create your models here.
+
+class Partido(models.Model):
+    idPartido=models.CharField(max_length=8, default="", primary_key=True,)
+    numLine=models.IntegerField()
+    numScrum=models.IntegerField()
+    numPenales=models.IntegerField()
+    tri=models.IntegerField()
+    numConversiones=models.IntegerField()
+    numDrop=models.IntegerField()
+    numTacles=models.IntegerField()
+    numMaul=models.IntegerField()
+    numRuck=models.IntegerField()
+    cantPases=models.IntegerField()
+
+class Error(models.Model):
+    idError = models.CharField(max_length=8, primary_key=True,default="")
+    numLinePerdidos=models.IntegerField()
+    numScrumPerdidos=models.IntegerField()
+    numPenalesEnContra=models.IntegerField()
+    numPelotasCaidas=models.IntegerField()
+    numConversionesErrados=models.IntegerField()
+    numTaclesErrados=models.IntegerField()
+    numPaseFoword=models.IntegerField()
+    numDropErrados=models.IntegerField()
+    numMaulErrados=models.IntegerField()
+    numRuckErrados=models.IntegerField()
+
+class Equipo(models.Model):
+    club=models.CharField(max_length=35, default="Introduzca nombre del club", primary_key=True,)
+    division=models.CharField(max_length=1, default="Introduzca division del club")
+    cantJugadores=models.IntegerField()
+    amonestados=models.IntegerField()
+    expulsados=models.IntegerField()
+
+class Jugador(models.Model):
+    nombre=models.CharField(max_length=25, default="")
+    apellido=models.CharField(max_length=25, default="")
+    dni=models.IntegerField()
+    tel=models.IntegerField()
+    posicion=models.CharField(max_length=50, default="")
+    asistenciaComp=models.BooleanField(default=False)
+    gym=models.BooleanField(default=False)
+    amonestado=models.BooleanField(default=False)
+    expulsado=models.BooleanField(default=False)
+
+class Entrenador(models.Model):
+    nombre=models.CharField(max_length=25, default="")
+    apellido=models.CharField(max_length=25, default="")
+    dni=models.IntegerField()
+    telefono=models.IntegerField()
+
+class Estadistica(models.Model):
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    partido = models.ForeignKey(Partido, on_delete=models.CASCADE)
+    error = models.ForeignKey(Error, on_delete=models.CASCADE)
